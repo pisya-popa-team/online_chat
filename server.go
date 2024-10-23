a9135500ab2ebea10c18c36c7cdd8ccac7cc0d0c
Sangiria <Kawaiinika@yandex.ru>
2024-10-23 20:51:25 +0300

    добавлена имплементация jwt токенов в ручки user

--- server.go
+++ server.go
@@ -1,18 +1,28 @@
 package main
 
 import (
+	"online_chat/enviroment"
 	"online_chat/handlers"
 
+	echojwt "github.com/labstack/echo-jwt/v4"
 	"github.com/labstack/echo/v4"
 )
 
 func main() {
 	e := echo.New()
-	
-	e.GET("/users/:id", handlers.GetUserByID)
-	e.POST("/users", handlers.CreateUser)
-	e.PUT("/users/:id", handlers.UpdateUser)
-	e.DELETE("/users/:id", handlers.DeleteUser)
+
+	auth := e.Group("/auth")
+
+	auth.Use(echojwt.WithConfig(echojwt.Config{
+		SigningKey: []byte(enviroment.GoDotEnvVariable("ACCESS_TOKEN_SECRET")),
+	}))
+
+	e.POST("/user", handlers.CreateUser)
+
+	auth.GET("/user/:id", handlers.GetUserByID)
+	auth.GET("/users", handlers.GetAllUsers)
+	auth.PUT("/user/:id", handlers.UpdateUser)
+	auth.DELETE("/user/:id", handlers.DeleteUser)
 
 	e.Logger.Fatal(e.Start(":1323"))
 }
