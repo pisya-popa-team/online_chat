cabe48b65793da05beaf52b8f35666c9e0648b25
Sangiria <Kawaiinika@yandex.ru>
2024-11-22 17:26:08 +0300

    fix: замена метода PUT на PATCH в user/update

--- routes/user.go
+++ routes/user.go
@@ -8,5 +8,5 @@ import (
 func InitUserRoutes(access *echo.Group) {
 	access.GET("/users/me", handlers.GetInfoAboutMe)
 	access.GET("/users", handlers.GetAllUsers)
-	access.PUT("/user/update", handlers.UpdateUser)
+	access.PATCH("/user/update", handlers.UpdateUser)
 }
