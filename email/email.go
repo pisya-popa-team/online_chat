1f320074384d321b35252dafbf3e2b323e7f331f
Sangiria <Kawaiinika@yandex.ru>
2024-11-25 16:42:34 +0300

    fix: перенос url в .env

--- email/email.go
+++ email/email.go
@@ -9,13 +9,14 @@ var (
 	email = enviroment.GoDotEnvVariable("EMAIL")
 	email_smtp = enviroment.GoDotEnvVariable("EMAIL_SMTP")
 	email_password = enviroment.GoDotEnvVariable("EMAIL_PASSWORD")
+	url = enviroment.GoDotEnvVariable("URL")
 )
 func EmailSender(user_email string, code string) error {
 	e := gomail.NewMessage()
 	e.SetHeader("From", email)
 	e.SetHeader("To", user_email)
 	e.SetHeader("Subject", "Ссылка для восстановления пароля")
-	e.SetBody("text/plain", "https://tt-chat.danyatochka.ru/recovery?token="+code)
+	e.SetBody("text/plain", url + code)
 
 	dialer := gomail.NewDialer(email_smtp, 587, email, email_password)
 
