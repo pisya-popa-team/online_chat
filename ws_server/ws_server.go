cb8d8715e9321780de4a6c3d27d0f17768f27968
Sangiria <Kawaiinika@yandex.ru>
2025-03-20 20:15:24 +0300

    fix: временные логи

--- ws_server/ws_server.go
+++ ws_server/ws_server.go
@@ -17,8 +17,9 @@ var upgrader = websocket.Upgrader{
 	CheckOrigin: func(r *http.Request) bool {
 		allowedOrigins := map[string]bool{
 			"http://localhost:4200": true,
-			"https://tt-chat.danyatochka.ru/": true,
+			"https://tt-chat.danyatochka.ru": true,
 		}
+		fmt.Println(r.Header.Get("Origin"))
 		return allowedOrigins[r.Header.Get("Origin")]
 	},
 }
